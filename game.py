2aeb6de4660185eb5ca509c36d974ce2a21bbaf6
nikhilkanzarkar <nikhilkanzarkar@utexas.edu>
2017-10-29 01:57:02 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,10 +1,14 @@
-#!/usr/bin/python -tt
-
 import pygame
 import random
 from sprite import Player
 from sprite import Chicken
 from sprite import Fries
+from sprite import Burger
+from sprite import apple
+from sprite import cherry
+from sprite import Spinach
+from sprite import pear
+from sprite import cookie
 
 BLACK = ( 0, 0, 0)
 WHITE = ( 255, 255, 255)
@@ -34,13 +38,15 @@ def main():
 
     clock = pygame.time.Clock()
 
-    title = pygame.image.load('images/StartLogo.png')
-    subtitle = pygame.image.load('images/Subtitle.png')
+    title = pygame.image.load('StartLogo.png')
+    subtitle = pygame.image.load('Subtitle.png')
 
     all_sprites_list = pygame.sprite.Group()
+    bad_foods_list = pygame.sprite.Group()
+    good_foods_list = pygame.sprite.Group()
 
     font = pygame.font.Font(None, 15)
-    text = font.render("WEIGHT", 1, (10, 10, 10))
+    text = font.render("HEALTH", 1, (10, 10, 10))
 
     player = Player(15, 25)
     player.rect.x = WIDTH/2
@@ -50,10 +56,10 @@ def main():
     while carryOn:
         for event in pygame.event.get(): # User did something
             if event.type == pygame.QUIT: # If user clicked close
-                pygame.quit()
+                carryOn = False
             elif event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_x: #Pressing 'x' Key will quit
-                     pygame.quit()
+                     carryOn=False
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player.rect.x > 0:
@@ -86,12 +92,51 @@ def main():
         all_sprites_list.update()
         all_sprites_list.draw(screen)
         pygame.display.flip()
-
         clock.tick(60)
 
+    sprite1 = Chicken(32, 32)
+    sprite1.rect.x = 125
+    sprite1.rect.y = 250
+    all_sprites_list.add(sprite1)
+    bad_foods_list.add(sprite1)
+    sprite2 = Fries(32, 32)
+    sprite2.rect.x = 40
+    sprite2.rect.y = 210
+    all_sprites_list.add(sprite2)
+    bad_foods_list.add(sprite2)
+    sprite3 = Burger(32, 32)
+    sprite3.rect.x = 400
+    sprite3.rect.y = 250
+    all_sprites_list.add(sprite3)
+    bad_foods_list.add(sprite3)
+    sprite4 = apple(32, 32)
+    sprite4.rect.x = 210
+    sprite4.rect.y = 250
+    good_foods_list.add(sprite4)
+    all_sprites_list.add(sprite4)
+    sprite5 = cherry(32, 32)
+    sprite5.rect.x = 295
+    sprite5.rect.y = 210
+    all_sprites_list.add(sprite5)
+    good_foods_list.add(sprite5)
+    sprite6 = pear(32, 32)
+    sprite6.rect.x = 470
+    sprite6.rect.y = 200
+    all_sprites_list.add(sprite6)
+    good_foods_list.add(sprite6)
+    sprite7 = Spinach(32, 32)
+    sprite7.rect.x = 570
+    sprite7.rect.y = 250
+    all_sprites_list.add(sprite7)
+    good_foods_list.add(sprite7)
+    sprite8 = cookie(32, 32)
+    sprite8.rect.x = 750
+    sprite8.rect.y = 210
+    all_sprites_list.add(sprite8)
+    bad_foods_list.add(sprite8)
     carryOn = True
     player.rect.x = 0
-    direction = 1   #tracks direction character faces
+    direction  = 1
     while carryOn:
 
         for event in pygame.event.get(): # User did something
@@ -104,11 +149,8 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player.rect.x > 0:
             player.moveLeft(4)
-            if direction > 0:
-                player.image = pygame.transform.flip(player.image, True, False)
-                direction = -1
 
-        if keys[pygame.K_RIGHT] and player.rect.x < WIDTH - 15:
+        if keys[pygame.K_RIGHT]:
             player.moveRight(4)
             if direction < 0:
                 player.image = pygame.transform.flip(player.image, True, False)
@@ -127,25 +169,14 @@ def main():
             flagger = 30
             flag2 = 0
 
-        collision_list = pygame.sprite.spritecollide(player,bad_food,False)
-        for food in collision_list:
-            obesityLevel += 1
-            bad_food.remove(food)
-            all_sprites_list.remove(food)
-
-        collision_list = pygame.sprite.spritecollide(player, good_food, False)
-        for food in collision_list:
-            obesityLevel -= 1
-            good_food.remove(food)
-            all_sprites_list.remove(food)
+        if keys[pygame.K_RIGHT]:
+            player.moveRight(4)
 
         screen.fill(MCYELLOW)
 
-
-        screen.blit(text, (WIDTH - 100,0))
+        screen.blit(text, (WIDTH - 100 ,0))
         pygame.draw.rect(screen, BLACK, [WIDTH - 50, 1, obesityLevel*5, 6])
-
-        pygame.draw.rect(screen, BROWN, [0, 270, WIDTH, 30],0)
+        pygame.draw.rect(screen, BROWN, [0, 270, WIDTH, 270],0)
         all_sprites_list.update()
         all_sprites_list.draw(screen)
         pygame.display.flip()
--- sprite.py
+++ sprite.py
@@ -1,14 +1,14 @@
 import pygame
 
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
-MCYELLOW = ( 226, 179, 9)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+MCYELLOW = (226, 179, 9)
 
 
 class Player(pygame.sprite.Sprite):
-    #This class represents a car. It derives from the "Sprite" class in Pygame.
+    # This class represents a car. It derives from the "Sprite" class in Pygame.
 
     def __init__(self, width, height):
         # Call the parent class (Sprite) constructor
@@ -21,7 +21,7 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey(WHITE)
 
         # Instead we could load a proper pciture of a car...
-        self.image = pygame.image.load("images/BoySprite3.png").convert_alpha()
+        self.image = pygame.image.load("BoySprite3.png").convert_alpha()
 
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
@@ -30,7 +30,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += pixels
 
     def jump(self, pixels):
-        self.rect.y -= pixels
+            self.rect.y -= pixels
 
     def moveLeft(self, pixels):
         self.rect.x -= pixels
@@ -48,13 +48,50 @@ class Chicken(pygame.sprite.Sprite):
         self.image.set_colorkey(WHITE)
 
         # Draw the car (a rectangle!)
-        self.image = pygame.image.load("images/chickenSmall.png").convert_alpha()
+        self.image = pygame.image.load("chickenSmall.png").convert_alpha()
 
         self.rect = self.image.get_rect()
 
     def fall(self, pixels):
         self.rect.y += pixels
 
+class Burger(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Draw the car (a rectangle!)
+        self.image = pygame.image.load("burger.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+
+    def fall(self, pixels):
+        self.rect.y += pixels
+
+class apple(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+
+        # Draw the car (a rectangle!)
+        self.image = pygame.image.load("apple.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+
+    def fall(self, pixels):
+        self.rect.y += pixels
 
 class Fries(pygame.sprite.Sprite):
     def __init__(self, width, height):
@@ -64,7 +101,67 @@ class Fries(pygame.sprite.Sprite):
         self.image.fill(MCYELLOW)
         self.image.set_colorkey(MCYELLOW)
 
-        self.image = pygame.image.load("images/FriesSprite.png").convert_alpha()
+        self.image = pygame.image.load("FriesSprite.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+
+    def fall(self, pixels):
+        self.rect.y += pixels
+
+class Spinach(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(MCYELLOW)
+        self.image.set_colorkey(MCYELLOW)
+
+        self.image = pygame.image.load("spinach2.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+
+    def fall(self, pixels):
+        self.rect.y += pixels
+
+class pear(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(MCYELLOW)
+        self.image.set_colorkey(MCYELLOW)
+
+        self.image = pygame.image.load("pear2.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+
+    def fall(self, pixels):
+        self.rect.y += pixels
+
+class cookie(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(MCYELLOW)
+        self.image.set_colorkey(MCYELLOW)
+
+        self.image = pygame.image.load("cookie2.png").convert_alpha()
+
+        self.rect = self.image.get_rect()
+
+    def fall(self, pixels):
+        self.rect.y += pixels
+
+class cherry(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(MCYELLOW)
+        self.image.set_colorkey(MCYELLOW)
+
+        self.image = pygame.image.load("cherry2.png").convert_alpha()
 
         self.rect = self.image.get_rect()
 
